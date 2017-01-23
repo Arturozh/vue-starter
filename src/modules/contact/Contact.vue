53a052c4618ccbd14817a452b88c0fac935fcf7f
jroji <1joroji@gmail.com>
2017-01-23 17:32:32 +0100

    first update

---
+++ src/modules/contact/Contact.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-bind:class="divClassName">
+    <img src="../../assets/logo.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      divClassName: 'contact'
+    }
+  }
+}
+</script>
+
+<style>
+  .contact {
+    text-align: center;
+  }
+</style>
